df6c8a871316be3ab56ccd99025d09a0e8821edb
Fabio Frasson <fabio.frass@gmail.com>
2021-05-03 10:40:43 -0300

    Static components insertion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import Navbar from "components/Navbar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Hello World!</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Hello World!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
