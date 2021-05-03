5fdf75aa93cdb519c7c3b28ff025f351affdeefa
Fabio Frasson <fabio.frass@gmail.com>
2021-05-03 09:49:44 -0300

    Project upload

---
+++ back-end/src/test/java/com/fabiofrasson/DsVendas/DsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiofrasson.DsVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
