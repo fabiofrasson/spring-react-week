17a514c4581110e62561ebcb4be6bed5b20abc50
Fabio Frasson <fabio.frass@gmail.com>
2021-05-07 11:58:42 -0300

    Home & Navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,30 @@
+import Footer from "components/Footer";
+import Navbar from "components/Navbar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
-    return (
-      <div className="">
-        <h1>Página Home</h1>
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">
+            Analise o desempenho das suas vendas por diferentes perspectivas
+          </p>
+          <hr />
+          <p>
+            Esta aplicação consiste em exibir um dashboard a partir de dados
+            fornecidos por um back end construído com Spring Boot.
+          </p>
+          <Link className="btn btn-primary btn-lg" to="/dashboard">
+              Acessar o Dashboard
+            </Link>
+        </div>
       </div>
-    );
-  };
-  
-  export default Home;
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
