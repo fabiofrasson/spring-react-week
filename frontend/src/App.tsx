71cf480d79bf1a2a3812cfd3aedd7dd7406b5750
Fabio Frasson <fabio.frass@gmail.com>
2021-05-07 11:51:37 -0300

    Routes config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,8 @@
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import DonutChart from "components/DonutChart";
-import Footer from "components/Footer";
-import Navbar from "components/Navbar";
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <div className="container">
-        <h1 className="text-primary py-3">Sales Dashboard</h1>
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Success Rate (%)</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Success Rate (%)</h5>
-            <DonutChart />
-          </div>
-        </div>
-        <div className="py-3">
-          <h2 className="text-primary">Gross Sales</h2>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+    <Routes />
   );
 }
 
